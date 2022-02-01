bc41519ee0aad56a2944bc40dd52628e1290faa3
UYK <meopulior@naver.com>
2022-02-01 20:23:08 +0900

    프로젝트 세팅

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Hello {
+
+    @Getter @Setter
+    private String data;
+    
+}
---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		Hello hello = new Hello();
+
+		hello.setData("HELLO");
+		String data = hello.getData();
+		System.out.println("DATA : " + data);
+	}
+
+}
